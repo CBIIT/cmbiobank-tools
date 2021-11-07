e342a8a20b37b4113b02bddc075559ea3990ac0e
Mark A. Jensen <mark.jensen@nih.gov>
2021-11-06 20:27:22 -0400

    feat(cmb products): algorithm using audit files
    WIP

--- rave-reduce/cmb-products.py
+++ rave-reduce/cmb-products.py
@@ -1,6 +1,9 @@
 import os
+import re
 import sys
 import shlex
+import logging
+import argparse
 from pathlib import Path
 from subprocess import run
 import yaml
@@ -8,9 +11,44 @@ from file_series import file_series as fs
 from yaml import CLoader as loader
 from pdb import set_trace
 
-conf = yaml.load(open("cmb-products.yaml","r"),Loader=loader)
+parser = argparse.ArgumentParser()
+parser.add_argument('--dry-run', action="store_true",
+                    help="log rave-reduce commands, but do not run them")
+parser.add_argument('--conf-file', default="cmb-products.yaml")
+parser.add_argument('--verbose','-v',action="count")
+parser.add_argument('--quiet','-q',action="count")
+parser.add_argument('--stage-dir', default=".cmb-build",help="directory for staging built products")
+
+sys.argv.extend(['--dry-run'])
+args = parser.parse_args()
+
+
+logging.basicConfig(style='{')
+logger = logging.getLogger("cmb-products")
+stage_dir = Path(args.stage_dir)
+
+if not stage_dir.exists():
+    stage_dir.mkdir()
+
+if not args.quiet and not args.verbose:
+    logger.setLevel(logging.WARNING)
+elif args.verbose==1:
+    logger.setLevel(logging.INFO)
+elif args.verbose > 1:
+    logger.setLevel(logging.DEBUG)
+elif args.quiet==1:
+    logger.setLevel(logging.ERROR)
+elif args.quiet > 1:
+    logger.setLevel(logging.CRITICAL)
+
+if args.dry_run:
+    logger.setLevel(logging.DEBUG)
+    
+logger.info("Loading config yaml")
+conf = yaml.load(open(args.conf_file,"r"),Loader=loader)
 locs = conf['paths']
 base = (Path(locs["base_path"]) if locs.get("base_path") else Path("."))
+logger.info("Base path is {}".format(base))
 
 # process locations (dirs) from yaml
 for loc in locs:
@@ -20,6 +58,7 @@ for loc in locs:
         locs[loc] = Path(locs[loc])
     else:
         locs[loc] = base / Path(locs[loc])
+    logger.debug("'{}' is at {}".format(loc, locs[loc]))
 
 # sources
 entity_ids = fs.FileSeries(locs["entity_id_source"])
@@ -35,36 +74,84 @@ iroc_local = fs.FileSeries(locs["iroc_local"])
 # workflow
 cmd = []
 
-# assume a requirement: given an entity_ids file datestamp, it has
-# incorporated all rave dump and vari inventory information with
-# datestamps earlier or equal to that date
-#
+# Each product has an associated .audit file. Each line in the
+# audit file is the dirname or filename of the input data merged
+# into the given product file via rave-reduce.
+
 # also assume that the entire historical series of all rave dumps and
 # all vari inventory files are in the source directories
 # so - rave_dumps.paths_until( entity_ids_rds.latest_date ).latest_path
 # is the last rave dump included in the latest entity ids file 
 
-rave_dumps_to_do = rave_dumps.paths_since(entity_ids_rds.latest_date)
-vari_inv_to_do = vari_inventory.paths_since(entity_ids_rds.latest_date)
+# update ids
 
-if rave_dumps_to_do or vari_inv_to_do:
-    
-    id_rds = entity_ids_rds.latest_path
-    rdump = rave_dumps.paths_until( entity_ids_rds.latest_date ).pop_latest()
+id_rds = entity_ids_rds.latest_path
+id_rds_date = entity_ids_rds.latest.date
+id_rds_tag = re.match("entity_ids[.](.*)[.]rds",id_rds.name).group(1)
 
-    if (rave_dumps_to_do.earliest_date < vari_inv_to_do.earliest_date):
-        # update ids
-        rdump = rave_dumps_to_do.pop_earliest()
+if (not id_rds_tag) or ( entity_ids_rds.latest_date.strftime("%Y%m%d") not in id_rds_tag):
+    logger.error("Entity id filename {} is non-standard; can't detect datestamp".format(id_rds.name))
+    raise RuntimeError("Entity id filename is non-standard: can't detect datestamp")
+
+logger.info("Entity ids starting rds file: {}".format(id_rds))
+
+id_rds_audit = id_rds.with_suffix(".audit")
+if not id_rds_audit.exists():
+    logger.error("No audit file {} found for starting entity id rds".format(id_rds_audit))
+    raise RuntimeException("No audit file found")
+
+with id_rds_audit.open() as f:
+    aud = [ x.rstrip() for x in f ]
+
+# rave dumps not present in audit file
+cur = { x[1].name for x in rave_dumps }
+rave_dumps_to_do = fs.FileSeries(seq=[ x for x in rave_dumps if x[1].name in cur-set(aud) ])
+# inventories not present in audit file
+cur = { x[1].name for x in vari_inventory }
+vari_inv_to_do = fs.FileSeries(seq=[ x for x in vari_inventory if x[1].name in cur-set(aud)])
+
+# merge new rave dumps, earliest to latest
+if rave_dumps_to_do:
+
+    for rdump in rave_dumps_to_do.iter_from_earliest():
+        logger.info("Merge rave dump {}".format(rdump[1].name))
         cmd = [ '../rave-reduce.r',
                 '-s','update_ids',
                 '--ids-file',str(id_rds),
                 '-d', shlex.quote(
                     rdump[0].strftime("%d %b %Y")),
                 str(rdump[1]) ]
-        # rc = run(cmd, capture_output=True, cwd=Path('try'))
-        id_rds = locs["entity_id_source"] / Path("entity_ids.{}.rds".format( rdump[0].strftime("%Y%m%d") ))
-    else:
-        vi = vari_inv_to_do.pop_earliest()
+        logger.debug("cmd: {}".format(" ".join(cmd)))
+        next_id_rds_tag = rdump[0].strftime("%Y%m%d")
+        while (stage_dir/Path("entity_ids.{}.rds".format(next_id_rds_tag))).exists():
+            mtch = re.match"^(202[0-9]{5})[.]([0-9]+)$",next_id_rds_tag)
+            if mtch.group(2):
+                next_id_rds_tag = "{}.{}".format(mtch.group(1),int(mtch.group(2))+1)
+            else:
+                next_id_rds_tag = "{}.1".format(mtch.group(1))
+        next_id_rds = stage_dir / Path("entity_ids.{}.rds".format(next_id_rds_tag))
+        logger.info("Creating intermediate id file {}".format(next_id_rds))
+        if not args.dry_run:
+            rc = run(cmd, capture_output=True, cwd=stage_dir)
+            try:
+                rc.check_returncode()
+            except subprocess.CalledProcessError as e:
+                logger.error("On run: {}\nstderr: {}\nstdout {}".format(
+                    " ".join(e.args), e.stderr, e.stdout))
+                raise e
+            (stage_dir / Path("entity_ids.update.rds")).rename(next_id_rds)
+        logger.debug("Add {} to audit list".format(rdump[1].name))
+        aud.extend( [rdump[1].name] )
+        id_rds = next_id_rds
+        id_rds_date = datetime.date(int(next_id_rds_tag[0:4]),
+                                    int(next_id_rds_tag[4:6]),
+                                    int(next_id_rds_tag[6:8]))                                    
+
+# then merge new var inventories, earliest to latest
+if vari_inv_to_do:
+
+    for vi in vari_inv_to_do.iter_from_earliest():
+        logger.info("Merge vari inventory {}".format(vi[1].name))
         cmd = [ '../rave-reduce.r',
                 '-s', 'update_ids',
                 '--ids-file',str(id_rds),
@@ -72,13 +159,41 @@ if rave_dumps_to_do or vari_inv_to_do:
                 '-d', shlex.quote(
                     vi[0].strftime("%d %b %Y")),
                 str(rdump[1])]
-        # rc = run(cmd, capture_output=True, cwd=Path('try'))                    
-        id_rds = locs["entity_id_source"] / Path("entity_ids.{}.rds".format( vi[0].strftime("%Y%m%d") ))
+        logger.debug("cmd: {}".format(" ".join(cmd)))
+        if vi[0] > id_rds_date:
+            next_id_rds_tag = vi[0].strftime("%Y%m%d")
+        else:
+            next_id_rds_tag = id_rds_date.strftime("%Y%m%d")
+        while (stage_dir/Path("entity_ids.{}.rds".format(next_id_rds_tag))).exists():
+            mtch = re.match"^(202[0-9]{5})[.]([0-9]+)$",next_id_rds_tag)
+            if mtch.group(2):
+                next_id_rds_tag = "{}.{}".format(mtch.group(1),int(mtch.group(2))+1)
+            else:
+                next_id_rds_tag = "{}.1".format(mtch.group(1))
+        next_id_rds = stage_dir / Path("entity_ids.{}.rds".format(next_id_rds_tag))
+        logger.info("Creating intermediate id file {}".format(next_id_rds))
+        if not args.dry_run:
+            rc = run(cmd, capture_output=True, cwd=stage_dir)
+            try:
+                rc.check_returncode()
+            except subprocess.CalledProcessError as e:
+                logger.error("On run: {}\nstderr: {}\nstdout {}".format(
+                    " ".join(e.args), e.stderr, e.stdout))
+                raise e
+            (stage_dir / Path("entity_ids.update.rds")).rename(next_id_rds)
+        logger.debug("Add {} to audit list".format(vi[1].name))
+        aud.extend( [ vi[1].name ] )
+        id_rds = next_id_rds
+        id_rds_date = datetime.date(int(next_id_rds_tag[0:4]),
+                                    int(next_id_rds_tag[4:6]),
+                                    int(next_id_rds_tag[6:8]))                                    
+# Now id_rds is the latest entity ids file - if any updates were performed
+# this is in the stage directory (otherwise, is the original in the source
+# directory
+# Create any required outgoing products in the stage directory:
 
 
-    pass
 
-pass
-            
+
 
 
